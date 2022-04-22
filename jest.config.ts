f4b264d64b5e70d6677f700af0b9fceb5283f0ab
Søren Bruus Zeppelin <sz@concordium.com>
2022-04-22 15:25:02 +0200

    working example test

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+    preset: 'ts-jest',
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
---
+++ packages/browser-wallet/test/example.test.ts
@@ -0,0 +1,5 @@
+test('jest works!', () => {
+    expect(1 + 1).toBe(2);
+});
+
+export {};
