f4c8a7db33b00cac18baa055f83150660dacb7a0
Søren Bruus Zeppelin <sz@concordium.com>
2022-04-26 11:16:19 +0200

    Memory router setup

--- packages/browser-wallet/src/popup/shell/Root/Root.tsx
+++ packages/browser-wallet/src/popup/shell/Root/Root.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { MemoryRouter, Link, Outlet, Route, Routes } from 'react-router-dom';
+
+function MainLayout() {
+    return (
+        <>
+            <main className="root root--emph">
+                Hello <span className="root__world">World!</span>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/setup">Setup</Link>
+            </nav>
+            <Outlet />
+        </>
+    );
+}
+
+function Home() {
+    return <>Home</>;
+}
+
+function Setup() {
+    return <>Setup</>;
+}
 
 export default function Root() {
     return (
-        <main className="root root--emph">
-            Hello <span className="root__world">World!</span>
-        </main>
+        <MemoryRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+                <Route path="/setup" element={<Setup />} />
+            </Routes>
+        </MemoryRouter>
     );
 }
