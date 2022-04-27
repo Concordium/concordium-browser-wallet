df14ede4a189aa338cfc393eb2d86b3e2f47c8dd
Søren Bruus Zeppelin <sz@concordium.com>
2022-04-27 15:58:29 +0200

    typed translations

---
+++ packages/browser-wallet/src/types/react-i18next.d.ts
@@ -0,0 +1,14 @@
+// import the original type declarations
+import 'react-i18next';
+import type { resources, defaultNS } from '../popup/shell/i18n';
+
+// react-i18next versions higher than 11.11.0
+declare module 'react-i18next' {
+    // and extend them!
+    interface CustomTypeOptions {
+        // custom namespace type if you changed it
+        defaultNS: typeof defaultNS;
+        // custom resources type
+        resources: typeof resources['en'];
+    }
+}
