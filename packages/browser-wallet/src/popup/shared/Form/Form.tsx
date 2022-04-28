3102c3724217df6317e0937e2070579d9b1a6751
Søren Bruus Zeppelin <sz@concordium.com>
2022-04-28 13:04:19 +0200

    Form component wrapping form hooks

---
+++ packages/browser-wallet/src/popup/shared/Form/Form.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    DefaultValues,
+    FieldValues,
+    SubmitHandler,
+    UseFormProps,
+    UseFormReturn,
+    useForm as useFormLib,
+} from 'react-hook-form';
+
+const useFormDefaults: Pick<UseFormProps, 'mode'> = {
+    mode: 'onTouched',
+};
+
+export const useForm = <TFormValues extends FieldValues = FieldValues>(
+    props?: UseFormProps<TFormValues>
+): UseFormReturn<TFormValues> => useFormLib<TFormValues>({ ...useFormDefaults, ...props });
+
+type FormProps<TFormValues> = {
+    onSubmit: SubmitHandler<TFormValues>;
+    formMethods?: UseFormReturn<TFormValues>;
+    defaultValues?: DefaultValues<TFormValues>;
+    children: (methods: UseFormReturn<TFormValues>) => React.ReactNode;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Form<TFormValues extends Record<string, any>>({
+    onSubmit,
+    formMethods: external,
+    defaultValues,
+    children,
+}: FormProps<TFormValues>) {
+    const internal = useForm<TFormValues>({ defaultValues });
+    const methods = external ?? internal;
+
+    return <form onSubmit={methods.handleSubmit(onSubmit)}>{children(methods)}</form>;
+}
