cd9c026aa511dbc6fb631e4dd5562aa89192952f
Morten Overgaard <overgaardmorten@gmail.com>
2022-05-02 23:04:33 +0200

    Added dummy test-event to be raised from popupmessageHandler

--- packages/browser-wallet/src/background/index.ts
+++ packages/browser-wallet/src/background/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { BackgroundMessageHandler } from '@concordium/browser-wallet-message-hub/src/message-handlers/background-messagehandler';
 import { PopupMessageHandler } from '@concordium/browser-wallet-message-hub/src/message-handlers/popup-messagehandler';
+import { HandlerTypeEnum, Message, MessageTypeEnum } from '@concordium/browser-wallet-message-hub';
 
 console.log('Background loaded');
 
@@ -12,6 +13,17 @@ const popUpHandler: PopupMessageHandler = new PopupMessageHandler();
 // Listen for all runtime port messages for the current selected tab
 backgroundHandler.addRuntimePortListenersForCurrentTab();
 popUpHandler.addRuntimePortListenersForCurrentTab();
+
+// Publish event
+setInterval(() => {
+    const eventMessage: Message = new Message(
+        HandlerTypeEnum.popupScript,
+        HandlerTypeEnum.injectedScript,
+        MessageTypeEnum.event,
+        { Event: 'The event' }
+    );
+    popUpHandler.publishMessage(eventMessage);
+}, 10000);
 
 // To force ESModule. Can safely be removed when any imports are added.
 export {};
