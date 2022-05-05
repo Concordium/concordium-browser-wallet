90fa15e299d191eb8484fbf116fb61c8e28af868
Morten Overgaard <overgaardmorten@gmail.com>
2022-05-05 10:57:33 +0200

    Removed code for Executing javascript into dApp main world and raising event instead

--- packages/browser-wallet-message-hub/src/message-handlers/background-messagehandler.ts
+++ packages/browser-wallet-message-hub/src/message-handlers/background-messagehandler.ts
@@ -1,26 +1,12 @@
-import { getCurrentTab } from '../shared/utils/extensionHelpers';
 import { HandlerTypeEnum } from './handlertype-enum';
 import { Message } from './message';
 import { logger } from './logger';
 import { MessageTypeEnum } from './messagetype-enum';
-import { AbstractExtensionMessageHandler } from './extension-messagehandler';
+import { AbstractWalletMessageHandler } from './abstract-wallet-messagehandler';
 
-export class BackgroundMessageHandler extends AbstractExtensionMessageHandler {
+export class BackgroundMessageHandler extends AbstractWalletMessageHandler {
     public constructor() {
         super(HandlerTypeEnum.backgroundScript);
-    }
-
-    private async injectWalletIntoDapp(): Promise<void> {
-        const tab: chrome.tabs.Tab = await getCurrentTab();
-
-        logger.log('Injecting InjectScript into dApp Context Main world');
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id! },
-            // TODO this is a reference to the output file, expecting to be placed in the root with manifest.json.
-            // Would be nice if the relative output path could be built from a reference to the entrypoint file instead.
-            files: ['inject.js'],
-            world: 'MAIN',
-        });
     }
 
     // Template method implementations
@@ -32,22 +18,8 @@ export class BackgroundMessageHandler extends AbstractExtensionMessageHandler {
     protected async handlePortMessageCoreInternal(message: Message, port: chrome.runtime.Port): Promise<void> {
         logger.log(`::BackgroundMessageHandler received ${JSON.stringify(message)}`);
 
-        // Init message --> Install Injected script
-        if (message.messageType === MessageTypeEnum.init) {
-            await this.injectWalletIntoDapp();
-        } else {
-            // TODO: Just for testing purposes - should be deleted when PopupMessageHandler is implemented
-            // Respond with message
-            const responseMessage: Message = new Message(
-                HandlerTypeEnum.backgroundScript,
-                HandlerTypeEnum.injectedScript,
-                MessageTypeEnum.sendTransaction,
-                {}
-            );
-            responseMessage.correlationId = message.correlationId;
-
-            await this.publishMessage(responseMessage);
-        }
+        // Raise event for listeners to handle
+        this.emit('message', message);
     }
 
     protected async handleWindowPostMessageCore(message: Message): Promise<void> {
