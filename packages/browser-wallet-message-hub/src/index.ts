dec4a150c38c0d7c08ccdf1057b21966c7142eba
movccd <102950990+movccd@users.noreply.github.com>
2022-05-05 22:58:34 +0200

    Update packages/browser-wallet-message-hub/src/index.ts
    Co-authored-by: Søren Bruus Zeppelin <sz@concordium.com>

--- packages/browser-wallet-message-hub/src/index.ts
+++ packages/browser-wallet-message-hub/src/index.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line no-console
-export const sendMessage = (message: string) => console.log('Message hub:', message);
-
 export { Message } from './message-handlers/message';
 export { AbstractMessageHandler } from './message-handlers/abstract-messagehandler';
 export { InjectedMessageHandler } from './message-handlers/injected-messagehandler';
