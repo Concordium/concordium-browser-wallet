69597eecc19fd09bfdd9479c36778cd0ddf0d214
Morten Overgaard <overgaardmorten@gmail.com>
2022-05-06 18:54:50 +0200

    Added example scripts to Zeppelin

--- packages/browser-wallet/src/background/index.ts
+++ packages/browser-wallet/src/background/index.ts
@@ -7,8 +7,6 @@ import { HandlerTypeEnum, Message, MessageTypeEnum } from '@concordium/browser-w
 
 console.log('Background loaded');
 
-const popup: PopupMessageHandler = new PopupMessageHandler();
-
 // Create BackgroundHandler which injects script into Dapp when asked.
 const backgroundHandler: BackgroundMessageHandler = new BackgroundMessageHandler();
 
@@ -20,7 +18,7 @@ backgroundHandler.on('message', async () => {
         throw new Error('No current Tab was found with a valid id.');
     }
 
-    logger.log('Injecting InjectScript into dApp Contextt Main world');
+    logger.log('Injecting InjectScript into dApp Context Main world');
     await chrome.scripting.executeScript({
         target: { tabId: tab.id },
         // TODO this is a reference to the output file, expecting to be placed in the root with manifest.json.
@@ -30,13 +28,36 @@ backgroundHandler.on('message', async () => {
     });
 });
 
+// *********** To Mr. Nazi-reviewer Der Zeppeliner :-) **********************'
+
+// This section is pure for demonstrating how events flow from Wallet to Dapp
+const popupHandler = new PopupMessageHandler();
+
+// Responding on sendTransaction message with correct correlationId
+// To send a transaction from the dapp do the following in the browser:
+// window.concordium.sendTransaction().then(console.log("We got a response"));
+popupHandler.on('message', async (m) => {
+    const response: Message = new Message(
+        HandlerTypeEnum.PopupScript,
+        HandlerTypeEnum.InjectedScript,
+        MessageTypeEnum.SendTransaction,
+        { data: 'sendTransaction' }
+    );
+    response.correlationId = m.correlationId;
+
+    await popupHandler.publishMessage(response);
+});
+
+// Raising events (AccountsChanged) to the current active window.
 setInterval(async () => {
     const message: Message = new Message(
         HandlerTypeEnum.PopupScript,
         HandlerTypeEnum.InjectedScript,
         MessageTypeEnum.Event,
-        {}
+        { payload: 'The data' }
     );
-    await popup.publishMessage(message);
-    console.log(`::popUpHandler ${message}`);
-}, 5000);
+    await popupHandler.publishMessage(message);
+
+    // For printing out message in the browser console
+    // window.concordium.on("event", (m)=>{console.log(JSON.stringify(m))});
+}, 10000);
