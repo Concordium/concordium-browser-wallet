fe6ad9b52119f9363df323c3022618543bb52844
Morten Overgaard <overgaardmorten@gmail.com>
2022-05-06 16:56:49 +0200

    Added logging info

--- packages/browser-wallet/src/content/index.ts
+++ packages/browser-wallet/src/content/index.ts
@@ -1,6 +1,8 @@
 import { HandlerTypeEnum, Message, MessageTypeEnum } from '@concordium/browser-wallet-message-hub';
 import { ContentMessageHandler } from '@concordium/browser-wallet-message-hub/src/message-handlers/content-messagehandler';
+import { logger } from '@concordium/browser-wallet-message-hub/src/message-handlers/logger';
 
+logger.log('Content loaded');
 // Create ContentMessageHandler and initialize ports and add event handlers
 const contentMessageHandler: ContentMessageHandler = new ContentMessageHandler();
 
