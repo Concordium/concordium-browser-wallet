01c00161070d73a5e235184870656d23cae34916
Søren Bruus Zeppelin <sz@concordium.com>
2022-05-12 10:13:28 +0200

    Example on how to use wallet api

--- packages/browser-wallet/src/inject/index.ts
+++ packages/browser-wallet/src/inject/index.ts
@@ -4,3 +4,5 @@ import { IWalletApi, walletApi } from '@concordium/browser-wallet-api';
 // Inject WalletAPI to dApp
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).concordium = walletApi as IWalletApi;
+
+(window as any).concordiumReady();
