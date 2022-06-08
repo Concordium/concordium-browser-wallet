4c36358b1fd94886533ab5a40de2c797d829c130
Søren Bruus Zeppelin <sz@concordium.com>
2022-06-08 13:04:21 +0200

    Configure storybook

---
+++ packages/browser-wallet/.storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+    core: {
+        builder: {
+            name: 'webpack5',
+            options: {
+                lazyCompilation: true,
+                fsCache: true,
+            },
+        },
+    },
+    stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/preset-scss',
+    ],
+    framework: '@storybook/react',
+};
