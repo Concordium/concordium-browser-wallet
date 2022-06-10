25dc13668785fcf4fa922dac798a1f29b57d36fa
Søren Bruus Zeppelin <sz@concordium.com>
2022-06-10 14:42:12 +0200

    dark mode support in storybook

---
+++ packages/browser-wallet/.storybook/preview.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+
+import './global.scss';
+import '../src/popup/shell/i18n';
+
+const themeDecorator = (Story, { globals, parameters }) => {
+    const darkValue = parameters.backgrounds.values.find((bg) => bg.name === 'dark')?.value;
+    const bgValue = globals.backgrounds?.value;
+    const isDark = bgValue === darkValue || (parameters.backgrounds.default === 'dark' && bgValue === undefined);
+
+    useEffect(() => {
+        const cl = document.getElementsByTagName('body').item(0)?.classList;
+
+        if (!cl) {
+            throw new Error('no body element');
+        }
+
+        if (isDark) {
+            cl.add('dark');
+        } else {
+            cl.remove('dark');
+        }
+    }, [isDark]);
+
+    return <Story />;
+};
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+export const decorators = [themeDecorator];
