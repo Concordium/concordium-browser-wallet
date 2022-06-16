2d91594046218f503b921cf52bcd005985e7823d
Søren Bruus Zeppelin <sz@concordium.com>
2022-06-16 10:33:54 +0200

    Header nav open state

--- packages/browser-wallet/src/popup/page-layouts/MainLayout/Header/Header.tsx
+++ packages/browser-wallet/src/popup/page-layouts/MainLayout/Header/Header.tsx
@@ -1,17 +1,27 @@
 import Button from '@popup/shared/Button';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Logo from '@assets/svg/concordium.svg';
+import clsx from 'clsx';
 
 export default function Header() {
     const { t } = useTranslation('mainLayout');
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="main-layout-header">
-            <Button className="main-layout-header__logo" clear>
-                <Logo />
-            </Button>
-            <h1>{t('title')}</h1>
+            <div className="main-layout-header__bar">
+                <Button className="main-layout-header__logo" clear onClick={() => setIsOpen((o) => !o)}>
+                    <Logo />
+                </Button>
+                <h1>{t('title')}</h1>
+            </div>
+            <nav className={clsx('main-layout-header__nav', isOpen && 'main-layout-header__nav--open')}>
+                <div>Accounts</div>
+                <div>Identities</div>
+                <div>Settings</div>
+            </nav>
         </header>
     );
 }
