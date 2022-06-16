c5045acdcca9aa13b76ff236c8b1a5dd0185944b
Søren Bruus Zeppelin <sz@concordium.com>
2022-06-16 16:21:19 +0200

    Remove initial nav

--- packages/browser-wallet/src/popup/page-layouts/MainLayout/MainLayout.tsx
+++ packages/browser-wallet/src/popup/page-layouts/MainLayout/MainLayout.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { NavLink, Outlet, Navigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { useAtom, useAtomValue } from 'jotai';
+import { Outlet, Navigate } from 'react-router-dom';
+import { useAtomValue } from 'jotai';
 
 import { absoluteRoutes } from '@popup/constants/routes';
-import { selectedAccountAtom, accountsAtom } from '@popup/store/account';
+import { accountsAtom } from '@popup/store/account';
 import Header from './Header';
 
 export default function MainLayout() {
-    const { t } = useTranslation('mainLayout');
     const accounts = useAtomValue(accountsAtom);
-    const [selectedAccount, setSelectedAccount] = useAtom(selectedAccountAtom);
 
     if (accounts.length === 0) {
         // Force user to go through setup
@@ -21,24 +18,7 @@ export default function MainLayout() {
         <div className="main-layout">
             <Header />
             <main className="main-layout__main">
-                <nav className="m-b-10">
-                    <NavLink to={absoluteRoutes.home.path}>{t('nav.home')}</NavLink> |{' '}
-                    <NavLink to={absoluteRoutes.setup.path}>{t('nav.setup')}</NavLink>
-                </nav>
-                <select
-                    className="main-layout__select-account"
-                    value={selectedAccount}
-                    onChange={(e) => setSelectedAccount(e.target.value)}
-                >
-                    {accounts.map((a) => (
-                        <option key={a} value={a}>
-                            {a}
-                        </option>
-                    ))}
-                </select>
-                <div className="m-t-10">
-                    <Outlet />
-                </div>
+                <Outlet />
             </main>
         </div>
     );
--- packages/browser-wallet/src/popup/pages/Account/Account.tsx
+++ packages/browser-wallet/src/popup/pages/Account/Account.tsx
@@ -1,12 +1,28 @@
-import { useAtomValue } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { selectedAccountAtom } from '@popup/store/account';
+import { accountsAtom, selectedAccountAtom } from '@popup/store/account';
 
 export default function Account() {
     const { t } = useTranslation('account');
-    const address = useAtomValue(selectedAccountAtom);
+    const accounts = useAtomValue(accountsAtom);
+    const [selectedAccount, setSelectedAccount] = useAtom(selectedAccountAtom);
 
-    return <div>{t('address', { address })}</div>;
+    return (
+        <>
+            <select
+                className="main-layout__select-account"
+                value={selectedAccount}
+                onChange={(e) => setSelectedAccount(e.target.value)}
+            >
+                {accounts.map((a) => (
+                    <option key={a} value={a}>
+                        {a}
+                    </option>
+                ))}
+            </select>
+            <div className="m-t-10">{t('address', { address: selectedAccount })}</div>
+        </>
+    );
 }
