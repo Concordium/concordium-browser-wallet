17178e9096814e68d324df55f7235142c078363f
Søren Bruus Zeppelin <sz@concordium.com>
2022-06-20 12:45:53 +0200

    Add remove account button to account page

--- packages/browser-wallet/src/popup/pages/Account/Account.tsx
+++ packages/browser-wallet/src/popup/pages/Account/Account.tsx
@@ -1,20 +1,32 @@
 import { useAtom, useAtomValue } from 'jotai';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
 import { accountsAtom, selectedAccountAtom } from '@popup/store/account';
 import { absoluteRoutes } from '@popup/constants/routes';
+import Button from '@popup/shared/Button';
+import { credentialsAtom } from '@popup/store/settings';
 
 export default function Account() {
     const { t } = useTranslation('account');
     const accounts = useAtomValue(accountsAtom);
     const [selectedAccount, setSelectedAccount] = useAtom(selectedAccountAtom);
+    const [creds, setCreds] = useAtom(credentialsAtom);
     const nav = useNavigate();
 
+    const removeAccount = useCallback(() => {
+        const next = creds.filter((c) => c.address !== selectedAccount);
+        setCreds(next);
+
+        if (next.length) {
+            setSelectedAccount(next[0].address);
+        }
+    }, [creds, selectedAccount]);
+
     return (
-        <>
-            <div className="flex">
+        <div className="flex-column justify-center align-center">
+            <div className="flex justify-space-between w-full">
                 <select
                     className="account-page__select-account"
                     value={selectedAccount}
@@ -31,6 +43,9 @@ export default function Account() {
                 </button>
             </div>
             <div className="account-page__address">{t('address', { address: selectedAccount })}</div>
-        </>
+            <Button faded className="m-t-20" onClick={removeAccount}>
+                Remove account (local only)
+            </Button>
+        </div>
     );
 }
