d3ac96889d2dcda4739252347743e42440099dc1
Søren Bruus Zeppelin <sz@concordium.com>
2022-06-29 09:50:43 +0200

    Add account settings page.

--- packages/browser-wallet/src/popup/pages/Account/Account.tsx
+++ packages/browser-wallet/src/popup/pages/Account/Account.tsx
@@ -1,20 +1,20 @@
 import { useAtom, useAtomValue } from 'jotai';
 import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 
 import { accountsAtom, selectedAccountAtom } from '@popup/store/account';
 import Button from '@popup/shared/Button';
 import { credentialsAtom, urlWhitelistAtom } from '@popup/store/settings';
 import CloseButton from '@popup/shared/CloseButton';
+import { absoluteRoutes } from '@popup/constants/routes';
 import { accountRoutes } from './routes';
 import AccountActions from './AccountActions';
 
-function Account() {
+function AccountSettings() {
     const { t } = useTranslation('account');
-    const accounts = useAtomValue(accountsAtom);
-    const [selectedAccount, setSelectedAccount] = useAtom(selectedAccountAtom);
     const [creds, setCreds] = useAtom(credentialsAtom);
+    const [selectedAccount, setSelectedAccount] = useAtom(selectedAccountAtom);
     const [whitelist, setWhitelist] = useAtom(urlWhitelistAtom);
 
     const removeAccount = useCallback(() => {
@@ -28,27 +28,51 @@ function Account() {
         setWhitelist([]);
     }, []);
 
+    if (selectedAccount === undefined) {
+        return null;
+    }
+
     return (
-        <div className="flex-column justify-space-between align-center h-full">
-            <div className="flex-column align-center">
+        <div className="flex-column">
+            <Button danger onClick={removeAccount}>
+                {t('removeAccount')}
+            </Button>
+            <Button disabled={!whitelist.length} danger className="m-t-20" onClick={removeConnections}>
+                {t('resetConnections')}
+            </Button>
+        </div>
+    );
+}
+
+function Account() {
+    const { t } = useTranslation('account');
+    const accounts = useAtomValue(accountsAtom);
+    const selectedAccount = useAtomValue(selectedAccountAtom);
+    const { pathname } = useLocation();
+    const nav = useNavigate();
+
+    const canClose = pathname !== absoluteRoutes.home.account.path;
+
+    return (
+        <div className="flex-column justify-space-between align-center h-full relative">
+            <div className="flex-column align-center flex-child-fill w-full">
                 {accounts.length === 0 && <div>{t('noAccounts')}</div>}
                 {selectedAccount !== undefined && (
                     <>
-                        <div className="account-page__address">{t('address', { address: selectedAccount })}</div>
-                        <Button danger className="m-t-20" onClick={removeAccount}>
-                            {t('removeAccount')}
-                        </Button>
+                        <div className="account-page__address">{selectedAccount}</div>
+                        <div className="account-page__routes">
+                            <Outlet />
+                            {canClose && (
+                                <CloseButton
+                                    className="account-page__close"
+                                    onClick={() => nav(absoluteRoutes.home.account.path)}
+                                />
+                            )}
+                        </div>
                     </>
                 )}
-                <Button disabled={!whitelist.length} danger className="m-t-20" onClick={removeConnections}>
-                    {t('resetConnections')}
-                </Button>
-                <div className="account-page__routes">
-                    <Outlet />
-                    <CloseButton />
-                </div>
             </div>
-            <AccountActions />
+            <AccountActions className="account-page__actions" />
         </div>
     );
 }
@@ -57,10 +81,10 @@ export default function AccountRoutes() {
     return (
         <Routes>
             <Route element={<Account />}>
-                <Route index element={<div>Transaction log</div>} />
+                <Route index element={<div className="">Transaction log</div>} />
                 <Route path={accountRoutes.send} element={<div>Send CCD</div>} />
                 <Route path={accountRoutes.receive} element={<div>Receive CCD</div>} />
-                <Route path={accountRoutes.settings} element={<div>Account settings</div>} />
+                <Route path={accountRoutes.settings} element={<AccountSettings />} />
             </Route>
         </Routes>
     );
--- packages/browser-wallet/src/popup/pages/Account/i18n/en.ts
+++ packages/browser-wallet/src/popup/pages/Account/i18n/en.ts
@@ -1,5 +1,4 @@
 const t = {
-    address: 'Address: {{address}}',
     noAccounts: 'No accounts in wallet',
     removeAccount: 'Remove account (local only)',
     resetConnections: 'Reset connections',
