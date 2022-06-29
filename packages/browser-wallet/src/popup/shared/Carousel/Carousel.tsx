4607ebeb3d2d3e4f51cfc52d48e1d21a10317e9f
Søren Bruus Zeppelin <sz@concordium.com>
2022-06-29 08:38:49 +0200

    Carousel elements

--- packages/browser-wallet/src/popup/shared/Carousel/Carousel.tsx
+++ packages/browser-wallet/src/popup/shared/Carousel/Carousel.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import Button from '../Button';
+import ButtonGroup from '../ButtonGroup';
 
 export default function Carousel() {
-    return <div>Carousel</div>;
+    return (
+        <div className="carousel">
+            <div className="carousel__content">Carousel</div>
+            <div className="carousel__divider" />
+            <div className="carousel__dots">dots</div>
+            <ButtonGroup className="carousel__actions">
+                <Button faded>Skip</Button>
+                <Button>Next</Button>
+            </ButtonGroup>
+        </div>
+    );
 }
