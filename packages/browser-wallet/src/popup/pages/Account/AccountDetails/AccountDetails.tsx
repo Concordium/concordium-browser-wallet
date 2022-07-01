210c1a220ee50ff9ccef904676cf4835a38f00ba
Søren Bruus Zeppelin <sz@concordium.com>
2022-07-01 13:13:28 +0200

    Account details component

--- packages/browser-wallet/src/popup/pages/Account/AccountDetails/AccountDetails.tsx
+++ packages/browser-wallet/src/popup/pages/Account/AccountDetails/AccountDetails.tsx
@@ -1,5 +1,21 @@
+import { displaySplitAddress } from '@popup/shared/utils/account-helpers';
 import clsx from 'clsx';
 import React from 'react';
+import { displayAsCcd } from 'wallet-common-helpers';
+
+type AmountProps = {
+    label: string;
+    amount: bigint;
+};
+
+function Amount({ label, amount }: AmountProps) {
+    return (
+        <div className="account-page-details__amount">
+            <div className="account-page-details__amount-label">{label}</div>
+            <div className="account-page-details__amount-ccd">{displayAsCcd(amount)}</div>
+        </div>
+    );
+}
 
 type Props = {
     expanded: boolean;
@@ -7,5 +23,15 @@ type Props = {
 };
 
 export default function AccountDetails({ expanded, account }: Props) {
-    return <div className={clsx('account-page-details', expanded && 'account-page-details--expanded')}>{account}</div>;
+    return (
+        <div className={clsx('account-page-details', expanded && 'account-page-details--expanded')}>
+            <div className="account-page-details__address">{displaySplitAddress(account)}</div>
+            <div className="account-page-details__id">Identity 1</div>
+            <div className="account-page-details__balance">
+                <Amount label="Public balance total" amount={0n} />
+                <Amount label="Public amount at disposal" amount={0n} />
+                <Amount label="Stake / delegation amount" amount={0n} />
+            </div>
+        </div>
+    );
 }
