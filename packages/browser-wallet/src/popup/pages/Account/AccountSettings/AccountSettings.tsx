bcbd3667fc2c3a18f3160d006a61d810712f0f60
orhoj <jo@concordium.com>
2022-08-25 09:47:36 +0200

    Add page for exporting private key

--- packages/browser-wallet/src/popup/pages/Account/AccountSettings/AccountSettings.tsx
+++ packages/browser-wallet/src/popup/pages/Account/AccountSettings/AccountSettings.tsx
@@ -4,18 +4,18 @@ import { Link, Route, Routes } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { accountSettingsRoutes } from './routes';
 import ConnectedSites from './ConnectedSites';
+import ExportPrivateKey from './ExportPrivateKey';
 
 export function AccountSettings() {
-    const { t: tc } = useTranslation('account', { keyPrefix: 'settings.connectedSites' });
     const { t } = useTranslation('account', { keyPrefix: 'settings' });
 
     return (
         <NavList className="account-settings-page">
             <Link className="account-settings-page__link" to={accountSettingsRoutes.connectedSites}>
-                {tc('title')}
+                {t('connectedSites.title')}
             </Link>
             <Link className="account-settings-page__link" to={accountSettingsRoutes.exportPrivateKey}>
-                {t('exportPrivateKey')}
+                {t('exportPrivateKey.title')}
             </Link>
         </NavList>
     );
@@ -26,7 +26,7 @@ export default function AccountSettingRoutes() {
         <Routes>
             <Route index element={<AccountSettings />} />
             <Route path={accountSettingsRoutes.connectedSites} element={<ConnectedSites />} />
-            <Route path={accountSettingsRoutes.exportPrivateKey} element={<div>Export private key</div>} />
+            <Route path={accountSettingsRoutes.exportPrivateKey} element={<ExportPrivateKey />} />
         </Routes>
     );
 }
