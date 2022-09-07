82c0b8cc33401b9f330cfd3fa3fe0930f1131bd5
Hjort <shj@concordium.com>
2022-09-07 14:54:52 +0200

    Fix errors

--- packages/browser-wallet/src/popup/page-layouts/FullscreenPromptLayout/FullscreenPromptLayout.tsx
+++ packages/browser-wallet/src/popup/page-layouts/FullscreenPromptLayout/FullscreenPromptLayout.tsx
@@ -9,7 +9,7 @@ import Toast from '@popup/shared/Toast/Toast';
 import { isSpawnedWindow } from '@popup/shared/window-helpers';
 import { useSelectedCredential } from '@popup/shared/utils/account-helpers';
 import AccountDetails from '@popup/pages/Account/AccountDetails';
-import AccountContext from '@popup/pages/Account/AccountContext';
+import AccountInfoListenerContext from '@popup/shared/AccountInfoListenerContext';
 
 type OnCloseHandler = () => void;
 type Unsubscribe = () => void;
@@ -105,13 +105,13 @@ export default function FullscreenPromptLayout() {
         <fullscreenPromptContext.Provider value={contextValue}>
             <Header />
             <div className="fullscreen-prompt-layout">
-                <AccountContext account={account}>
+                <AccountInfoListenerContext>
                     {account && <AccountDetails expanded={false} account={account} />}
                     <main className="fullscreen-prompt-layout__main">
                         <Outlet />
                     </main>
                     <Toast />
-                </AccountContext>
+                </AccountInfoListenerContext>
             </div>
         </fullscreenPromptContext.Provider>
     );
