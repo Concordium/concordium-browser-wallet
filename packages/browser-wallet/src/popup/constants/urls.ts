047d6435b2af84e899171d88ccda0c60eaa73c15
Jakob Ørhøj <jo@concordium.com>
2022-09-19 10:09:56 +0200

    Update link to license notices

--- packages/browser-wallet/src/popup/constants/urls.ts
+++ packages/browser-wallet/src/popup/constants/urls.ts
@@ -1,5 +1,6 @@
 const urls = {
     termsAndConditions: 'https://developer.concordium.software/en/mainnet/net/resources/terms-and-conditions-bw.html',
+    licenseAttributions: 'https://developer.concordium.software/en/mainnet/net/resources/bw-licenses.html',
     website: 'https://www.concordium.com',
     supportWebsite: 'https://support.concordium.software',
     documentationWebsite: 'https://developer.concordium.software/en/mainnet/index.html',
