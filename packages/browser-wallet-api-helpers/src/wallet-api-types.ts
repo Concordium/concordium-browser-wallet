e4f7b6eb2cbce64aec5d22fd4638995b9a09159f
Hjort <shj@concordium.com>
2022-09-22 16:34:04 +0200

    Remove parameters from smart contract payload types in api

--- packages/browser-wallet-api-helpers/src/wallet-api-types.ts
+++ packages/browser-wallet-api-helpers/src/wallet-api-types.ts
@@ -2,9 +2,16 @@ import type {
     AccountTransactionPayload,
     AccountTransactionSignature,
     AccountTransactionType,
+    InitContractPayload,
     JsonRpcClient,
     SchemaVersion,
+    UpdateContractPayload,
 } from '@concordium/web-sdk';
+
+type SendTransactionPayload =
+    | Exclude<AccountTransactionPayload, UpdateContractPayload | InitContractPayload>
+    | Omit<UpdateContractPayload, 'parameter'>
+    | Omit<InitContractPayload, 'parameter'>;
 
 /**
  * An enumeration of the events that can be emitted by the WalletApi.
@@ -36,7 +43,7 @@ interface MainWalletApi {
      * Note that if the user rejects signing the transaction, this will throw an error.
      * @param accountAddress the address of the account that should sign the transaction
      * @param type the type of transaction that is to be signed and sent.
-     * @param payload the payload of the transaction to be signed and sent.
+     * @param payload the payload of the transaction to be signed and sent. Note that for smart contract transactions, the payload should not contain the parameters, those should instead be provided in the subsequent argument instead.
      * @param parameters parameters for the initContract and updateContract transactions in JSON-like format.
      * @param schema schema used for the initContract and updateContract transactions to serialize the parameters. Should be base64 encoded.
      * @param schemaVersion version of the schema provided. Must be supplied for schemas that use version 0 or 1, as they don't have the version embedded.
@@ -46,7 +53,7 @@ interface MainWalletApi {
         type:
             | AccountTransactionType.UpdateSmartContractInstance
             | AccountTransactionType.InitializeSmartContractInstance,
-        payload: AccountTransactionPayload,
+        payload: SendTransactionPayload,
         parameters: Record<string, unknown>,
         schema: string,
         schemaVersion?: SchemaVersion
@@ -56,12 +63,12 @@ interface MainWalletApi {
      * Note that if the user rejects signing the transaction, this will throw an error.
      * @param accountAddress the address of the account that should sign the transaction
      * @param type the type of transaction that is to be signed and sent.
-     * @param payload the payload of the transaction to be signed and sent.
+     * @param payload the payload of the transaction to be signed and sent. Note that for smart contract transactions, the payload should not contain the parameters, those should instead be provided in the subsequent argument instead.
      */
     sendTransaction(
         accountAddress: string,
         type: AccountTransactionType,
-        payload: AccountTransactionPayload
+        payload: SendTransactionPayload
     ): Promise<string>;
     /**
      * Sends a message to the Concordium Wallet and awaits the users action. If the user signs the message, this will resolve to the signature.
