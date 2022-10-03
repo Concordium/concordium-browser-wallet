a00860c077b7b09f563d5e71fde2caf5da74cdd6
Doris Benda <DOBEN@users.noreply.github.com>
2022-10-03 11:16:18 +0300

    Fix linting wCCD example

--- examples/wCCD/src/utils.ts
+++ examples/wCCD/src/utils.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { createContext } from 'react';
 import { detectConcordiumProvider } from '@concordium/browser-wallet-api-helpers';
-import { AccountTransactionType, GtuAmount } from '@concordium/web-sdk';
+import { AccountTransactionType, GtuAmount, toBuffer } from '@concordium/web-sdk';
 
 export const CONTRACT_NAME_PROXY = 'CIS2-wCCD-Proxy';
 export const CONTRACT_NAME_IMPLEMENTATION = 'CIS2-wCCD';
@@ -39,6 +39,7 @@ export const wrap = (
                         },
                         receiveName: `${CONTRACT_NAME_PROXY}.wrap`,
                         maxContractExecutionEnergy: 30000n,
+                        parameter: toBuffer(''),
                     },
                     {
                         data: '',
@@ -94,6 +95,7 @@ export const unwrap = (
                         },
                         receiveName: `${CONTRACT_NAME_PROXY}.unwrap`,
                         maxContractExecutionEnergy: 30000n,
+                        parameter: toBuffer(''),
                     },
                     {
                         amount: amount.toString(),
