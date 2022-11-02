dfc2273b74f2f040adb314cee171f57fc191ccb5
Hjort <shj@concordium.com>
2022-11-02 15:20:28 +0100

    Remove create-react-app from voting example

--- examples/voting/src/App.js
+++ examples/voting/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import {Navbar} from "react-bootstrap";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import CreateElectionPage from "./CreateElectionPage";
