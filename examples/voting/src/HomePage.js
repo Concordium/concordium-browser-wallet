fc986255362e39ecf6d0fdbc570897c7297cace7
Doris Benda <DOBEN@users.noreply.github.com>
2022-11-08 10:52:11 +0200

    Add watching for changes

--- examples/voting/src/HomePage.js
+++ examples/voting/src/HomePage.js
@@ -1,30 +1,29 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import {Button, Col, Container, Row} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const HomePage = (props) => {
+function HomePage() {
     return (
         <Container>
-          <Row>
-            <Col className="text-center m-4">
-              <Row>
-                <h1>Voting 3000-100</h1>
-              </Row>
-              <Row>
-                <h3 className="text-muted">Make votes great again</h3>
-              </Row>
-            </Col>
-          </Row>
-          <Row>
-            <img src="static/img/trump-voting.jpg" alt="How to vote."/>
-          </Row>
-          <Row>
-            <Col className="text-center mt-5">
-              <Link to="/create"><Button className="btn bg-success text-white font-weight-bold py-3 px-4"><strong>Create a <u>YUGE</u> election!</strong></Button></Link>
-            </Col>
-          </Row>
+            <Row>
+                <Col className="text-center m-4">
+                    <Row>
+                        <h1>Voting Application</h1>
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="text-center mt-5">
+                    <Link to="/create">
+                        <Button className="btn bg-success text-white font-weight-bold py-3 px-4">
+                            <strong>Setup election</strong>
+                        </Button>
+                    </Link>
+                </Col>
+            </Row>
         </Container>
     );
-};
+}
 
 export default HomePage;
--- examples/voting/src/VotePage.js
+++ examples/voting/src/VotePage.js
@@ -1,12 +1,15 @@
-import React, {useEffect, useMemo, useState} from 'react';
-import {useParams, Link} from "react-router-dom";
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
-import Wallet, {castVote, getVotes, init} from "./Wallet";
-import {decodeVotingView} from "./buffer";
+/* eslint-disable no-console */
+/* eslint-disable consistent-return */
+/* eslint-disable react/jsx-filename-extension */
+import React, { useEffect, useMemo, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import Wallet, { castVote, getVotes, init } from './Wallet';
+import { decodeVotingView } from './buffer';
 
-const VotePage = (props) => {
+function VotePage() {
     const params = useParams();
-    const {electionId} = params;
+    const { electionId } = params;
 
     const [client, setClient] = useState();
     const [connectedAccount, setConnectedAccount] = useState();
@@ -15,35 +18,22 @@ const VotePage = (props) => {
     const [selectedOption, setSelectionOption] = useState();
 
     // Attempt to initialize Browser Wallet Client.
-    useEffect(
-        () => {
-            init(setConnectedAccount)
-                .then(setClient)
-                .catch(console.error);
-        },
-        [],
-    );
+    useEffect(() => {
+        init(setConnectedAccount).then(setClient).catch(console.error);
+    }, []);
 
     // Fetch votes from contract.
-    useEffect(
-        () => {
-            if (client) {
-                getVotes(client, electionId)
-                    .then(setGetvotesResult)
-                    .catch(console.error)
-            }
-        },
-        [client, electionId]
-    );
+    useEffect(() => {
+        if (client) {
+            getVotes(client, electionId).then(setGetvotesResult).catch(console.error);
+        }
+    }, [client, electionId]);
 
-    const votes = useMemo(
-        () => {
-            if (getvotesResult) {
-                return decodeVotingView(getvotesResult.returnValue);
-            }
-        },
-        [getvotesResult],
-    );
+    const votes = useMemo(() => {
+        if (getvotesResult) {
+            return decodeVotingView(getvotesResult.returnValue);
+        }
+    }, [getvotesResult]);
 
     return (
         <Container>
@@ -58,8 +48,7 @@ const VotePage = (props) => {
             </Row>
             <Row>
                 <Col>
-                    <h1>Vote in Election #{electionId}</h1>
-                    <div>Guaranteed to be non-fraudulent!</div>
+                    <h1>Vote in Election {electionId}*</h1>
                 </Col>
             </Row>
             <Row>
@@ -69,40 +58,50 @@ const VotePage = (props) => {
             </Row>
             <Row>
                 <Col>
-                <Form>
+                    <Form>
                         <div className="mb-3">
-                        {votes?.opts.map(
-                            v =>
-                                <Form.Check
-                                key={v}
-                                type="radio"
-                                id={`default-radio-` + v}>
-                                    <Form.Check.Input className="btn-check" type="radio" onChange={() => setSelectionOption(v)} checked={selectedOption === v}/>
+                            {votes?.opts.map((v) => (
+                                <Form.Check key={v} type="radio" id={`default-radio-${v}`}>
+                                    <Form.Check.Input
+                                        className="btn-check"
+                                        type="radio"
+                                        onChange={() => setSelectionOption(v)}
+                                        checked={selectedOption === v}
+                                    />
                                     <Form.Check.Label className="btn btn-light w-100">{v}</Form.Check.Label>
                                 </Form.Check>
-                        )}
+                            ))}
                         </div>
                         <Button
                             className="w-100"
-                            onClick={() => castVote(client, electionId, votes?.opts.indexOf(selectedOption), connectedAccount)}
-                        ><strong>Cast Vote!</strong></Button>
-
+                            onClick={() =>
+                                castVote(client, electionId, votes?.opts.indexOf(selectedOption), connectedAccount)
+                            }
+                        >
+                            <strong>Cast Vote!</strong>
+                        </Button>
                     </Form>
-                    <ul>
-                    </ul>
+                    <ul />
                 </Col>
             </Row>
             <Row>
                 <Col>
-                <Link to={"/results/" + electionId}>
-                <Button className="btn-secondary font-weight-bold">
-                    <strong>Strong Results!</strong>
-                </Button>
-                </Link>
+                    <Link to={`/results/${electionId}`}>
+                        <Button className="btn-secondary font-weight-bold">
+                            <strong>Results</strong>
+                        </Button>
+                    </Link>
                 </Col>
             </Row>
+            <br />
+            <br />
+            <br />
+            <br />
+            <footer>
+                <p>*Smart contract index on the Concordium chain</p>
+            </footer>
         </Container>
     );
-};
+}
 
 export default VotePage;
