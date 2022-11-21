3f01e12e64c598873c30b9c676ede79ce5acbb89
Søren Bruus Zeppelin <sz@concordium.com>
2022-11-21 13:27:16 +0100

    Proper icons

--- packages/browser-wallet/src/popup/pages/IdProofRequest/DisplayStatement/DisplayStatement.tsx
+++ packages/browser-wallet/src/popup/pages/IdProofRequest/DisplayStatement/DisplayStatement.tsx
@@ -7,6 +7,9 @@ import SecretIcon from '@assets/svg/id-secret.svg';
 import RevealIcon from '@assets/svg/id-reveal.svg';
 import InfoTooltipIcon from '@assets/svg/info-tooltip.svg';
 import WarningTooltipIcon from '@assets/svg/warning-tooltip.svg';
+import CheckmarkIcon from '@assets/svg/checkmark-dark-green.svg';
+import CrossIcon from '@assets/svg/cross.svg';
+
 import Button from '@popup/shared/Button';
 import Modal from '@popup/shared/Modal';
 
@@ -23,8 +26,13 @@ function DisplayStatementLine({ attribute, value, isRequirementMet }: StatementL
         <li className="display-statement__line">
             <div>{attribute}:</div>
             <div className="display-statement__line-separator" />
-            <div>
-                {value} {isRequirementMet ? '√' : 'x' /* TODO: change to proper icons */}
+            <div className="flex align-center">
+                {value}
+                {isRequirementMet ? (
+                    <CheckmarkIcon className="display-statement__line-check" />
+                ) : (
+                    <CrossIcon className="display-statement__line-cross" />
+                )}
             </div>
         </li>
     );
@@ -54,7 +62,7 @@ function StatementTooltip({ reveal }: StatementTooltipProps) {
             onOpen={() => setOpen(true)}
             onClose={() => setOpen(false)}
             trigger={
-                <Button clear>
+                <Button clear className="flex">
                     {reveal ? (
                         <WarningTooltipIcon className="display-statement__tooltip-icon" />
                     ) : (
@@ -91,7 +99,7 @@ export function DisplayStatementView({ className, lines, dappName, header, ...pr
     const { t } = useTranslation('idProofRequest', { keyPrefix: 'displayStatement' });
 
     return (
-        <section className={clsx('display-statement', className)}>
+        <section className={clsx('display-statement', !isValid && 'display-statement--invalid', className)}>
             <header className="display-statement__header">
                 <div className="flex align-center">
                     {props.reveal ? (
@@ -103,7 +111,19 @@ export function DisplayStatementView({ className, lines, dappName, header, ...pr
                         <div>
                             <strong>{header}</strong>
                         </div>
-                        <div>{isValid ? `${t('requirementsMet')} √` : `${t('requirementsNotMet')} X`}</div>
+                        <div className="flex align-center">
+                            {isValid ? (
+                                <>
+                                    {t('requirementsMet')}{' '}
+                                    <CheckmarkIcon className="display-statement__requirements-icon" />
+                                </>
+                            ) : (
+                                <>
+                                    {t('requirementsNotMet')}{' '}
+                                    <CrossIcon className="display-statement__requirements-icon" />
+                                </>
+                            )}
+                        </div>
                     </div>
                 </div>
                 <StatementTooltip reveal={props.reveal} />
