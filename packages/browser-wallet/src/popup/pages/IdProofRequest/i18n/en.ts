01318d3bf73cc56539618cd378318119a0b7666e
Hjort <shj@concordium.com>
2022-12-13 10:55:06 +0100

    Fix errors in text on Id Proof popup

--- packages/browser-wallet/src/popup/pages/IdProofRequest/i18n/en.ts
+++ packages/browser-wallet/src/popup/pages/IdProofRequest/i18n/en.ts
@@ -6,10 +6,10 @@ export default {
         requirementsMet: 'You meet this requirement',
         requirementsNotMet: "You don't meet this requirement",
         revealDescription:
-            '<1>Important:</1> {{dappName}} will be given all the information above. You should only accept to do so, if you have absolute trust in the service, and if you are familiar with their privacy policiy.',
+            '<1>Important:</1> {{dappName}} will be given all the information above. You should only accept, if you have absolute trust in the service, and if you are familiar with their privacy policy.',
         revealTooltip: {
             header: 'Revealing information <1 />',
-            body: 'When you reveal information for a third party, you effectively hand over the information to them. This means that you should only do this if you have absolute trust in them, and if you are familiar with their data usage and protection procedures.\n\nYou can read more in on\n\ndeveleoper.concordium.software',
+            body: 'When you reveal information to a third party, you effectively hand over the information to them. This means that you should only do this if you have absolute trust in them, and if you are familiar with their data usage and protection procedures.\n\nYou can read more in on\n\ndeveleoper.concordium.software',
         },
         secretTooltip: {
             header: 'Zero Knowledge proofs',
