ff3fade014d362ba5858f93523ee12eec27d694d
Søren Bruus Zeppelin <sz@concordium.com>
2022-12-14 16:25:01 +0100

    Add carousel to delegate page

--- packages/browser-wallet/src/popup/pages/Account/Earn/Delegate/Delegate.tsx
+++ packages/browser-wallet/src/popup/pages/Account/Earn/Delegate/Delegate.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Carousel from '@popup/shared/Carousel';
+
+const routes = {
+    intro: 'intro',
+    register: 'register',
+    preUdpate: 'pre-update',
+    update: 'update',
+    preRemove: 'pre-remove',
+    remove: 'remove',
+};
+
+function Intro() {
+    const nav = useNavigate();
+    return (
+        <Carousel onContinue={() => nav(routes.register)}>
+            <div>First</div>
+            <div>Second</div>
+            <div>Third</div>
+        </Carousel>
+    );
+}
 
 export default function Delegate() {
-    return <>Delegate</>;
+    return <Intro />;
 }
