ff08b3d3108ab609ae38dc6f30c379dbbdf0a2e0
Søren Bruus Zeppelin <sz@concordium.com>
2022-12-14 11:31:58 +0100

    Statement modal fixes + make external links look clickable

--- packages/browser-wallet/src/popup/pages/IdProofRequest/i18n/da.ts
+++ packages/browser-wallet/src/popup/pages/IdProofRequest/i18n/da.ts
@@ -11,11 +11,11 @@ const da: typeof en = {
             '<1>Vigtigt:</1> {{dappName}} får adgang til alt information der står ovenfor. Du skal kun acceptere at afsløre informationen, hvis du har tillid til servicen og hvis du er bekendt med deres privathedspolitik.',
         revealTooltip: {
             header: 'Afslører information <1 />',
-            body: 'Når du afslører information til en tredjepart, kan de beholde denne information. Dette betyder, at du kun bør afsløre information til dem hvis du stoler på dem, og er bekendt med deres databrugs- samt databeskyttelses-procedurer.\n\n Du kan læse mere på\n\n<1>developer.concordium.software</1>',
+            body: 'Når du afslører information til en tredjepart, kan de beholde denne information. Dette betyder, at du kun bør afsløre information til dem hvis du er enig med deres databrugs- samt databeskyttelses-politik.\n\n Du kan læse mere i\n\n<1>vores udvikler dokumentation</1>',
         },
         secretTooltip: {
             header: 'Zero Knowledge beviser',
-            body: 'Zero Knowledge beviser er en måde at bevise noget overfor en service eller dApp, uden at afsløre den underliggende personlige information. Et eksempel kan være, at du beviser at du er over 18 år gammel, uden at bevise din specifikke fødselsdato. Et andet eksempel kan være, at du bor i ét ud af en række lande, uden at afsløre hvilken af disse lande du bor i.\n\nDu kan læse mere på\n\n<1>developer.concordium.software</1>',
+            body: 'Zero Knowledge beviser er en måde at bevise noget overfor en service eller dApp, uden at afsløre den underliggende personlige information. Et eksempel kan være, at du beviser at du er over 18 år gammel, uden at bevise din specifikke fødselsdato. Et andet eksempel kan være, at du bor i ét ud af en række lande, uden at afsløre hvilken af disse lande du bor i.\n\nDu kan læse mere i\n\n<1>vores udvikler dokumentation</1>',
         },
         headers: {
             reveal: 'Information der afsløres',
