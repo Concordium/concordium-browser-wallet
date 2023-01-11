51f7af24936c9e8fd8fed7911fc28295ce2f2e51
Doris Benda <DOBEN@users.noreply.github.com>
2023-01-11 16:50:29 +0100

    Add display of file record

--- examples/eSealing/src/Root.tsx
+++ examples/eSealing/src/Root.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { SignClientTypes } from '@walletconnect/types';
-import { version } from '../package.json';
 
-import WCCD from './wCCD';
+import SEALING from './eSealing';
 import { WithWalletConnection } from './wallet/WithWalletConnection';
 import { TESTNET, WALLET_CONNECT_PROJECT_ID } from './constants';
 import { Network } from './wallet/WalletConnection';
@@ -11,8 +10,8 @@ const network: Network = TESTNET;
 const walletConnectOpts: SignClientTypes.Options = {
     projectId: WALLET_CONNECT_PROJECT_ID,
     metadata: {
-        name: 'wCCD',
-        description: 'Example dApp for the wCCD token.',
+        name: 'eSealing',
+        description: 'Example dApp for the eSealing a file.',
         url: '#',
         icons: ['https://walletconnect.com/walletconnect-logo.png'],
     },
@@ -26,21 +25,10 @@ export default function Root() {
         <div>
             <script src="../../node_modules/@concordium/web-sdk/lib/concordium.min.js" />
             <script src="../../packages/browser-wallet-api-helpers/lib/concordiumHelpers.min.js" />
-            <main className="wccd">
+            <main className="eSealing">
                 <WithWalletConnection network={network} walletConnectOpts={walletConnectOpts}>
-                    {(props) => <WCCD {...props} />}
+                    {(props) => <SEALING {...props} />}
                 </WithWalletConnection>
-                <div>
-                    Version: {version} |{' '}
-                    <a
-                        style={{ color: 'white' }}
-                        href="https://developer.concordium.software/en/mainnet/smart-contracts/tutorials/wCCD/index.html"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Learn how to make a wrapper like this
-                    </a>
-                </div>
             </main>
         </div>
     );
--- examples/eSealing/src/utils.ts
+++ examples/eSealing/src/utils.ts
@@ -2,13 +2,7 @@
 
 import { createContext } from 'react';
 import { AccountTransactionType, UpdateContractPayload, CcdAmount } from '@concordium/web-sdk';
-import {
-    CONTRACT_NAME,
-    E_SEALING_CONTRACT_NAME,
-    E_SEALING_RAW_SCHEMA,
-    WRAP_FUNCTION_RAW_SCHEMA,
-    UNWRAP_FUNCTION_RAW_SCHEMA,
-} from './constants';
+import { E_SEALING_CONTRACT_NAME, E_SEALING_RAW_SCHEMA } from './constants';
 import { WalletConnection } from './wallet/WalletConnection';
 
 /**
@@ -39,84 +33,6 @@ export async function register(
         // @ts-ignore
         fileHashByteArray,
         E_SEALING_RAW_SCHEMA
-    );
-}
-
-/**
- * Action for wrapping some CCD to WCCD in the WCCD smart contract instance
- */
-export async function wrap(
-    connection: WalletConnection,
-    account: string,
-    index: bigint,
-    subindex = 0n,
-    amount = 0,
-    receiver = account
-) {
-    if (!Number.isInteger(amount) || amount <= 0) {
-        throw new Error('invalid amount');
-    }
-
-    return connection.signAndSendTransaction(
-        account,
-        AccountTransactionType.Update,
-        {
-            amount: new CcdAmount(BigInt(amount)),
-            address: {
-                index,
-                subindex,
-            },
-            receiveName: `${CONTRACT_NAME}.wrap`,
-            maxContractExecutionEnergy: 30000n,
-        } as UpdateContractPayload,
-        {
-            data: '',
-            to: {
-                Account: [receiver],
-            },
-        },
-        WRAP_FUNCTION_RAW_SCHEMA
-    );
-}
-
-/**
- * Action for unwrapping some WCCD to CCD in the WCCD smart contract instance
- */
-export async function unwrap(
-    connection: WalletConnection,
-    account: string,
-    index: bigint,
-    subindex = 0n,
-    amount = 0,
-    receiver = account
-) {
-    if (!Number.isInteger(amount) || amount <= 0) {
-        throw new Error('invalid amount');
-    }
-
-    return connection.signAndSendTransaction(
-        account,
-        AccountTransactionType.Update,
-        {
-            amount: new CcdAmount(BigInt(0)),
-            address: {
-                index,
-                subindex,
-            },
-            receiveName: `${CONTRACT_NAME}.unwrap`,
-            maxContractExecutionEnergy: 30000n,
-        },
-        {
-            amount: amount.toString(),
-            data: '',
-            owner: {
-                Account: [account],
-            },
-            receiver: {
-                Account: [receiver],
-            },
-        },
-        UNWRAP_FUNCTION_RAW_SCHEMA
     );
 }
 
