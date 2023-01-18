0d37d5de87ee40b8c5e9e4ee220c1c4ae4d945d1
Michael Bisgaard Olesen <mo@concordium.com>
2023-01-18 08:49:21 +0100

    Fix

--- examples/wCCD/src/constants.ts
+++ examples/wCCD/src/constants.ts
@@ -1,5 +1,5 @@
 // The TESTNET_GENESIS_BLOCK_HASH is used to check that the user has its browser wallet connected to testnet and not to mainnet.
-import { Network } from './wallet/WalletConnection';
+import { Network } from '@concordium/react-components';
 
 export const TESTNET_GENESIS_BLOCK_HASH = '4221332d34e1694168c2a0c0b3fd0f273809612cb13d000d5c2e00e85f50f796';
 
@@ -30,4 +30,5 @@ export const TESTNET: Network = {
     name: 'testnet',
     genesisHash: TESTNET_GENESIS_BLOCK_HASH,
     jsonRpcUrl: 'https://json-rpc.testnet.concordium.com',
+    ccdScanBaseUrl: 'https://testnet.ccdscan.io',
 };
