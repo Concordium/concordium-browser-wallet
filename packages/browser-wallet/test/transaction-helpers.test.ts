358aeec4e71e2e4ae3f623c8c2a32669e99bdb3b
Hjort <shj@concordium.com>
2023-03-13 13:17:04 +0100

    Fix validateTransferAmount handling of decimals

--- packages/browser-wallet/test/transaction-helpers.test.ts
+++ packages/browser-wallet/test/transaction-helpers.test.ts
@@ -1,12 +1,23 @@
 import { validateTransferAmount } from '../src/popup/shared/utils/transaction-helpers';
+import en from '../src/popup/shared/i18n/en';
 
-test('validateTransfer amount respects decimals parameter', () => {
+test('validateTransfer amount respects decimals paraimeter', () => {
+    const invalidMessage = en.utils.ccdAmount.invalid;
     expect(validateTransferAmount('1000', undefined, 0)).toBe(undefined);
-    expect(validateTransferAmount('1000.1', undefined, 0)).toContain('not a valid');
+    expect(validateTransferAmount('1000.1', undefined, 0)).toContain(invalidMessage);
     expect(validateTransferAmount('1000.001', undefined, 3)).toBe(undefined);
-    expect(validateTransferAmount('1000.0001', undefined, 3)).toContain('not a valid');
+    expect(validateTransferAmount('1000.0001', undefined, 3)).toContain(invalidMessage);
     expect(validateTransferAmount('1000.000001', undefined, 6)).toBe(undefined);
-    expect(validateTransferAmount('1000.0000001', undefined, 6)).toContain('not a valid');
+    expect(validateTransferAmount('1000.0000001', undefined, 6)).toContain(invalidMessage);
     expect(validateTransferAmount('1000.000000000000000001', undefined, 18)).toBe(undefined);
-    expect(validateTransferAmount('1000.0000000000000000001', undefined, 18)).toContain('not a valid');
+    expect(validateTransferAmount('1000.0000000000000000001', undefined, 18)).toContain(invalidMessage);
+});
+
+test('validateTransfer does not allow letters in string', () => {
+    const invalidMessage = en.utils.ccdAmount.invalid;
+    expect(validateTransferAmount('a', undefined, 0)).toContain(invalidMessage);
+    expect(validateTransferAmount('1n', undefined, 0)).toContain(invalidMessage);
+    expect(validateTransferAmount('O1', undefined, 0)).toContain(invalidMessage);
+    expect(validateTransferAmount('5a2', undefined, 0)).toContain(invalidMessage);
+    expect(validateTransferAmount('test', undefined, 0)).toContain(invalidMessage);
 });
