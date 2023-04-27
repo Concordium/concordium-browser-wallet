3a254e1063dba7016aa58b33bfed845c71fb3fd5
Hjort <shj@concordium.com>
2023-04-27 13:57:15 +0200

    Add missing translation

--- packages/browser-wallet/src/popup/shared/i18n/da.ts
+++ packages/browser-wallet/src/popup/shared/i18n/da.ts
@@ -163,6 +163,7 @@ const t: typeof en = {
     continue: 'Fortsæt',
     okay: 'Okay',
     notice: 'Bemærk',
+    estimatedTransactionFee: 'Estimerede transaktionsomkostninger',
 };
 
 export default t;
