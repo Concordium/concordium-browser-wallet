1eed80122a2ddd232f0c72f0d5585c92ce28c76c
orhoj <jo@concordium.com>
2023-09-01 16:13:09 +0200

    Better display of unable to prove

--- packages/browser-wallet/src/popup/pages/Web3ProofRequest/Web3ProofRequest.tsx
+++ packages/browser-wallet/src/popup/pages/Web3ProofRequest/Web3ProofRequest.tsx
@@ -34,13 +34,15 @@ import {
 } from '@popup/store/verifiable-credential';
 import { useConfirmedIdentities } from '@popup/shared/utils/identity-helpers';
 import { parse } from '@shared/utils/payload-helpers';
-import { VerifiableCredential, VerifiableCredentialStatus } from '@shared/storage/types';
+import { VerifiableCredential, VerifiableCredentialStatus, WalletCredential } from '@shared/storage/types';
 import { getVerifiableCredentialStatus } from '@shared/utils/verifiable-credential-helpers';
 import { noOp, useAsyncMemo } from 'wallet-common-helpers';
 import { stringify } from '@concordium/browser-wallet-api/src/util';
 import CloseIcon from '@assets/svg/cross.svg';
 import {
     getAccountCredentialCommitmentInput,
+    getAccountCredentialsWithMatchingIssuer,
+    getActiveWeb3IdCredentialsWithMatchingIssuer,
     getViableAccountCredentialsForStatement,
     getViableWeb3IdCredentialsForStatement,
     getWeb3CommitmentInput,
@@ -76,37 +78,70 @@ async function getAllCredentialStatuses(
     return Object.fromEntries(statuses);
 }
 
+function findCredentialsForStatementIssuer(
+    statement: CredentialStatement,
+    credentials: WalletCredential[],
+    verifiableCredentials: VerifiableCredential[],
+    statuses: Record<string, VerifiableCredentialStatus | undefined>
+) {
+    if (isAccountCredentialStatement(statement)) {
+        return getAccountCredentialsWithMatchingIssuer(statement, credentials);
+    }
+
+    if (isVerifiableCredentialStatement(statement)) {
+        return getActiveWeb3IdCredentialsWithMatchingIssuer(statement, verifiableCredentials, statuses);
+    }
+
+    return undefined;
+}
+
 function DisplayNotProvable({
     onClick,
     dappName,
     statement,
     net,
+    statuses,
 }: {
     onClick: () => void;
     dappName: string;
     statement: CredentialStatement;
     net: Network;
+    statuses: Record<string, VerifiableCredentialStatus | undefined>;
 }) {
     const { t } = useTranslation('web3IdProofRequest');
     const credentials = useAtomValue(credentialsAtom);
     const verifiableCredentials = useAtomValue(storedVerifiableCredentialsAtom);
-    const validCredentials = isAccountCredentialStatement(statement) ? credentials : verifiableCredentials.value;
+    const validCredentials = findCredentialsForStatementIssuer(
+        statement,
+        credentials,
+        verifiableCredentials.value,
+        statuses
+    );
 
     return (
         <ExternalRequestLayout>
             <div className="web3-id-proof-request__statement-container">
-                <p className="web3-id-proof-request__not-provable-description bodyM ">
-                    {t('descriptions.unableToProve')}
-                </p>
-                <DisplayCredentialStatement
-                    className="m-t-10:not-first"
-                    dappName={dappName}
-                    validCredentials={validCredentials}
-                    credentialStatement={statement}
-                    net={net}
-                    setChosenId={noOp}
-                    showDescription={false}
-                />
+                {validCredentials && validCredentials?.length === 0 && (
+                    <p className="web3-id-proof-request__not-provable-description bodyM ">
+                        {t('descriptions.noCredentialsForThatIssuer')}
+                    </p>
+                )}
+                {validCredentials && validCredentials?.length > 0 && (
+                    <>
+                        <p className="web3-id-proof-request__not-provable-description bodyM ">
+                            {t('descriptions.unableToProve')}
+                        </p>
+                        <DisplayCredentialStatement
+                            className="m-t-10:not-first"
+                            dappName={dappName}
+                            validCredentials={validCredentials}
+                            credentialStatement={statement}
+                            net={net}
+                            setChosenId={noOp}
+                            showDescription={false}
+                        />
+                    </>
+                )}
                 <div className="web3-id-proof-request__actions flex">
                     <Button className="web3-id-proof-request__not-provable-button flex-child-fill" onClick={onClick}>
                         {t('reject')}
@@ -244,6 +279,7 @@ export default function Web3ProofRequest({ onReject, onSubmit }: Props) {
                 dappName={dappName}
                 net={net}
                 statement={statements[validCredentials.findIndex((v) => !v.length)]}
+                statuses={statuses}
             />
         );
     }
--- packages/browser-wallet/src/popup/pages/Web3ProofRequest/i18n/en.ts
+++ packages/browser-wallet/src/popup/pages/Web3ProofRequest/i18n/en.ts
@@ -42,6 +42,8 @@ export default {
         accountCredential:
             'Select an account associated with the identity whose credentials will be used to reveal/prove the requested information.',
         unableToProve: 'One or more attributes do not meet the requirements from the verifier.',
+        noCredentialsForThatIssuer:
+            'You do not hold any active credentials from the issuer that the verifier requested.',
     },
     failedProof: 'Unable to create proof',
     failedProofReason: 'Unable to create proof due to: {{ reason }}',
