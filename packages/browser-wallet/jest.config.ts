f21318d44e1fd6b83ad4563c123be8dfe3c0f953
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-10-17 13:55:56 +0200

    Fix tests

--- packages/browser-wallet/jest.config.ts
+++ packages/browser-wallet/jest.config.ts
@@ -2,15 +2,18 @@ import { pathsToModuleNameMapper } from 'ts-jest';
 import type { Config } from '@jest/types';
 import tsconfig from './tsconfig.json';
 
+const esModules = ['@concordium/web-sdk', '@concordium/rust-bindings', '@noble/ed25519', 'wallet-common-helpers'].join(
+    '|'
+);
+
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/js-with-ts-esm',
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: '<rootDir>/' }),
-        '@concordium/web-sdk': '@concordium/common-sdk',
         '^uuid$': 'uuid',
     },
-    transformIgnorePatterns: ['/node_modules/(?!@concordium/common-sdk)'],
+    transformIgnorePatterns: [`node_modules/(?!${esModules})`],
     modulePaths: ['<rootDir>/../../node_modules', '<rootDir>/node_modules'],
     setupFiles: ['./test/setup.ts'],
 };
