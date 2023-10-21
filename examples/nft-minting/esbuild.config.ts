16183eafaa64b38611414a205fe81a5a1037cffb
Søren Bruus Zeppelin <soerenbf@gmail.com>
2023-10-21 09:20:37 +0200

    Fix missing build + more gzip

---
+++ examples/nft-minting/esbuild.config.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+import esbuild, { BuildOptions } from 'esbuild';
+import { htmlPlugin } from '@craftamap/esbuild-plugin-html';
+import svgrPlugin from 'esbuild-plugin-svgr';
+import fs from 'fs';
+
+const watch = Boolean(process.env.WATCH);
+
+const main = 'src/index.tsx';
+
+const htmlTemplate = fs.readFileSync('src/index.html').toString();
+const htmlOut = 'index.html';
+
+const config: BuildOptions = {
+    entryPoints: [main],
+    entryNames: '[name]',
+    bundle: true,
+    minify: true,
+    metafile: true,
+    logLevel: 'info',
+    sourcemap: 'inline',
+    target: ['chrome67'],
+    outdir: 'dist',
+    plugins: [
+        htmlPlugin({
+            files: [
+                {
+                    entryPoints: [main],
+                    filename: htmlOut,
+                    htmlTemplate,
+                },
+            ],
+        }),
+        svgrPlugin(),
+    ],
+};
+
+(async () => {
+    if (watch) {
+        const ctx = await esbuild.context(config);
+        await ctx.watch();
+        console.log('watching for changes...');
+    } else {
+        esbuild.build(config).catch(() => process.exit(1));
+    }
+})();
