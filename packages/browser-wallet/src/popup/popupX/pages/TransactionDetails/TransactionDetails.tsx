ca93784f591143df2c3d60bad9120dcdd071ec3d
Emil Holm Gjørup <eg@concordium.com>
2024-10-08 15:30:00 +0200

    Support CCDScan links for transaction details

--- packages/browser-wallet/src/popup/popupX/pages/TransactionDetails/TransactionDetails.tsx
+++ packages/browser-wallet/src/popup/popupX/pages/TransactionDetails/TransactionDetails.tsx
@@ -6,25 +6,29 @@ import Text from '@popup/popupX/shared/Text';
 import Page from '@popup/popupX/shared/Page';
 import Button from '@popup/popupX/shared/Button';
 import { Navigate, useLocation } from 'react-router-dom';
+import * as CcdScan from '@popup/shared/utils/ccdscan';
 import { TransactionLogEntry } from '../TransactionLog/TransactionLog';
 
 /** State passed as part of the navigation */
 type LocationState = {
     /** Transaction information. */
-    transaction: TransactionLogEntry
-}
+    transaction: TransactionLogEntry;
+};
 
 type TransactionDetailsProps = {
-    transaction: TransactionLogEntry
-}
+    transaction: TransactionLogEntry;
+};
 
-function TransactionDetails({transaction}: TransactionDetailsProps) {
+function TransactionDetails({ transaction }: TransactionDetailsProps) {
     const copyTransactionHash = useCallback(() => navigator.clipboard.writeText(transaction.hash), []);
+    const seeOnCcdScan = useCallback(() => {
+        CcdScan.openTransaction(transaction.hash);
+    }, []);
     return (
         <Page className="transaction-details-x">
             <Page.Top heading="Transaction details">
                 <Button.Icon icon={<Copy />} onClick={copyTransactionHash} />
-                {/*<Button.Icon icon={<ArrowSquareOut />} disabled />*/}
+                <Button.Icon icon={<ArrowSquareOut />} onClick={seeOnCcdScan} />
             </Page.Top>
             <Page.Main>
                 <Card>
@@ -32,7 +36,9 @@ function TransactionDetails({transaction}: TransactionDetailsProps) {
                         <div className="transaction-details__card_row">
                             <div className="top-info">
                                 <Text.Label>{transaction.type}</Text.Label>
-                                <Text.Label className={transaction.income ? "income": ""}>{transaction.amount}</Text.Label>
+                                <Text.Label className={transaction.income ? 'income' : ''}>
+                                    {transaction.amount}
+                                </Text.Label>
                             </div>
                             <div className="top-info">
                                 <Text.Capture>
@@ -42,26 +48,22 @@ function TransactionDetails({transaction}: TransactionDetailsProps) {
                             </div>
                         </div>
                     </Card.Row>
-                    {transaction.fromAddress === undefined ? null : <Card.RowDetails title="From address" value={transaction.fromAddress} />}
-                    {transaction.toAddress === undefined ? null : <Card.RowDetails title="To address" value={transaction.toAddress} />}
-                    <Card.RowDetails
-                        title="Transaction hash"
-                        value={transaction.hash}
-                    />
-                    <Card.RowDetails
-                        title="Block hash"
-                        value={transaction.block}
-                    />
+                    {transaction.fromAddress === undefined ? null : (
+                        <Card.RowDetails title="From address" value={transaction.fromAddress} />
+                    )}
+                    {transaction.toAddress === undefined ? null : (
+                        <Card.RowDetails title="To address" value={transaction.toAddress} />
+                    )}
+                    <Card.RowDetails title="Transaction hash" value={transaction.hash} />
+                    <Card.RowDetails title="Block hash" value={transaction.block} />
                     <Card.Row>
                         <div className="transaction-details__card_row">
                             <Text.Capture>Events</Text.Capture>
-                            {transaction.events.map((event)=> (
-                                <span className="secondary-info" key={event} >
-                                    <Text.Capture>
-                                        {event}
-                                    </Text.Capture>
-                                </span>)
-                            )}
+                            {transaction.events.map((event) => (
+                                <span className="secondary-info" key={event}>
+                                    <Text.Capture>{event}</Text.Capture>
+                                </span>
+                            ))}
                         </div>
                     </Card.Row>
                 </Card>
@@ -70,13 +72,12 @@ function TransactionDetails({transaction}: TransactionDetailsProps) {
     );
 }
 
-export default function Loader () {
+export default function Loader() {
     const location = useLocation();
-    if (typeof location.state !== 'object' || location.state === null || !("transaction" in location.state)) {
+    if (typeof location.state !== 'object' || location.state === null || !('transaction' in location.state)) {
         // No transaction passed as part of the navigation state.
         return <Navigate to="../" />;
-    } else {
-        const state = location.state as LocationState;
-        return <TransactionDetails transaction={state.transaction}/>;
     }
+    const state = location.state as LocationState;
+    return <TransactionDetails transaction={state.transaction} />;
 }
