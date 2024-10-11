10273f5e25543b7f54dcbccc6a1f685a190db6e6
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-10-11 11:26:12 +0200

    Remove danish locale for popupX

--- packages/browser-wallet/src/popup/shell/i18n/locales/da.ts
+++ packages/browser-wallet/src/popup/shell/i18n/locales/da.ts
@@ -24,14 +24,13 @@ import connectAccountsRequest from '@popup/pages/ConnectAccountsRequest/i18n/da'
 import web3IdProofRequest from '@popup/pages/Web3ProofRequest/i18n/da';
 import verifiableCredential from '@popup/pages/VerifiableCredential/i18n/da';
 import addWeb3IdCredential from '@popup/pages/AddWeb3IdCredential/i18n/da';
+
 // TODO dansk
 import verifiableCredentialBackup from '@popup/pages/VerifiableCredentialBackup/i18n/en';
 import ageProofRequest from '@popup/pages/AgeProofRequest/i18n/da';
 import viewSeedPhrase from '@popup/pages/ViewSeedPhrase/i18n/da';
 
-import type en from './en';
-
-const t: typeof en = {
+const t = {
     shared,
     mainLayout,
     account,
