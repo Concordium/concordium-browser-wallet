a099cd99bb9e2fc1136f327ebaad3d04b42bc684
Emil Holm Gjørup <eg@concordium.com>
2024-10-23 13:03:12 +0200

    List actual accounts in Accounts page

--- packages/browser-wallet/src/popup/popupX/pages/Accounts/Accounts.tsx
+++ packages/browser-wallet/src/popup/popupX/pages/Accounts/Accounts.tsx
@@ -13,34 +13,86 @@ import Text from '@popup/popupX/shared/Text';
 import { useNavigate } from 'react-router-dom';
 import { absoluteRoutes } from '@popup/popupX/constants/routes';
 import { copyToClipboard } from '@popup/popupX/shared/utils/helpers';
+import { useAtomValue } from 'jotai';
+import { credentialsAtom } from '@popup/store/account';
+import { WalletCredential } from '@shared/storage/types';
+import { useIdentityName } from '@popup/shared/utils/account-helpers';
+import { useAccountInfo } from '@popup/shared/AccountInfoListenerContext';
+import { displayAsCcd } from 'wallet-common-helpers';
 
-const ACCOUNT_LIST = [
-    {
-        account: 'Account 1',
-        address: 'bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh',
-        balance: '4,227.38',
-        attached: 'Identity 1',
-    },
-    {
-        account: 'Account 2',
-        address: 'bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh',
-        balance: '4,227.38',
-        attached: 'Identity 1',
-    },
-    {
-        account: 'Account 3',
-        address: 'tt2kgdygjrsqtzq2n0yrf2493p83kkfjh50eo',
-        balance: '1,195.41',
-        attached: 'Identity 2',
-    },
-];
+function fallbackAccountName(credentialNumber: number): string {
+    return `Account ${1 + credentialNumber}`;
+}
 
-export default function Accounts() {
+type AccountListItemProps = {
+    credential: WalletCredential;
+};
+
+function AccountListItem({ credential }: AccountListItemProps) {
     const { t } = useTranslation('x', { keyPrefix: 'accounts' });
     const nav = useNavigate();
     const navToPrivateKey = () => nav(absoluteRoutes.settings.accounts.privateKey.path);
     const navToConnectedSites = () => nav(absoluteRoutes.settings.accounts.connectedSites.path);
     const navToIdCards = () => nav(absoluteRoutes.settings.idCards.path);
+
+    const identityName = useIdentityName(credential);
+    const accountInfo = useAccountInfo(credential);
+
+    const accountName = credential.credName !== '' ? credential.credName : fallbackAccountName(credential.credNumber);
+    const { address } = credential;
+    const ccdBalance =
+        accountInfo === undefined ? 'Loading' : displayAsCcd(accountInfo.accountAmount.microCcdAmount, false);
+    return (
+        <Card key={accountName}>
+            <Card.Row>
+                <Text.Main>{accountName}</Text.Main>
+                <Button.Icon className="transparent" icon={<Pencil />} />
+            </Card.Row>
+            <Card.Row>
+                <Text.Capture>{address}</Text.Capture>
+                <Button.Icon className="transparent" onClick={() => copyToClipboard(address)} icon={<Copy />} />
+            </Card.Row>
+            <Card.Row>
+                <Text.MainRegular>{t('ccdBalance')}</Text.MainRegular>
+                <Text.MainMedium>{ccdBalance}</Text.MainMedium>
+            </Card.Row>
+            <Card.Row>
+                <Text.MainRegular>{t('connectedSites')}</Text.MainRegular>
+                <Button.IconText
+                    className="transparent"
+                    onClick={navToConnectedSites}
+                    label={t('seeList')}
+                    icon={<ArrowRight />}
+                    leftLabel
+                />
+            </Card.Row>
+            <Card.Row>
+                <Text.MainRegular>{t('privateKey')}</Text.MainRegular>
+                <Button.IconText
+                    className="transparent"
+                    onClick={navToPrivateKey}
+                    label={t('export')}
+                    icon={<ArrowRight />}
+                    leftLabel
+                />
+            </Card.Row>
+            <Card.Row>
+                <Text.MainRegular>{t('attachedTo')}</Text.MainRegular>
+                <Button.IconText
+                    className="transparent"
+                    onClick={navToIdCards}
+                    label={identityName}
+                    icon={<ArrowRight />}
+                    leftLabel
+                />
+            </Card.Row>
+        </Card>
+    );
+}
+
+export default function Accounts() {
+    const { t } = useTranslation('x', { keyPrefix: 'accounts' });
+    const accounts = useAtomValue(credentialsAtom);
     return (
         <Page className="accounts-x">
             <Page.Top heading={t('accounts')}>
@@ -49,55 +101,8 @@ export default function Accounts() {
                 <Button.Icon icon={<Plus />} />
             </Page.Top>
             <Page.Main>
-                {ACCOUNT_LIST.map(({ account, address, balance, attached }) => (
-                    <Card key={account}>
-                        <Card.Row>
-                            <Text.Main>{account}</Text.Main>
-                            <Button.Icon className="transparent" icon={<Pencil />} />
-                        </Card.Row>
-                        <Card.Row>
-                            <Text.Capture>{address}</Text.Capture>
-                            <Button.Icon
-                                className="transparent"
-                                onClick={() => copyToClipboard(address)}
-                                icon={<Copy />}
-                            />
-                        </Card.Row>
-                        <Card.Row>
-                            <Text.MainRegular>{t('totalBalance')}</Text.MainRegular>
-                            <Text.MainMedium>{balance} USD</Text.MainMedium>
-                        </Card.Row>
-                        <Card.Row>
-                            <Text.MainRegular>{t('connectedSites')}</Text.MainRegular>
-                            <Button.IconText
-                                className="transparent"
-                                onClick={navToConnectedSites}
-                                label={t('seeList')}
-                                icon={<ArrowRight />}
-                                leftLabel
-                            />
-                        </Card.Row>
-                        <Card.Row>
-                            <Text.MainRegular>{t('privateKey')}</Text.MainRegular>
-                            <Button.IconText
-                                className="transparent"
-                                onClick={navToPrivateKey}
-                                label={t('export')}
-                                icon={<ArrowRight />}
-                                leftLabel
-                            />
-                        </Card.Row>
-                        <Card.Row>
-                            <Text.MainRegular>{t('attachedTo')}</Text.MainRegular>
-                            <Button.IconText
-                                className="transparent"
-                                onClick={navToIdCards}
-                                label={attached}
-                                icon={<ArrowRight />}
-                                leftLabel
-                            />
-                        </Card.Row>
-                    </Card>
+                {accounts.map((item) => (
+                    <AccountListItem credential={item} key={item.address} />
                 ))}
             </Page.Main>
         </Page>
