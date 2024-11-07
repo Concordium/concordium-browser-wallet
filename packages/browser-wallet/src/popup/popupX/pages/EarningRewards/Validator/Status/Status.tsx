3b20b4b45304a306c2628b58e6984bc9231e71eb
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-11-07 12:15:43 +0100

    Section validator status by stake and pool info

--- packages/browser-wallet/src/popup/popupX/pages/EarningRewards/Validator/Status/Status.tsx
+++ packages/browser-wallet/src/popup/popupX/pages/EarningRewards/Validator/Status/Status.tsx
@@ -8,10 +8,11 @@ import Button from '@popup/popupX/shared/Button';
 import Card from '@popup/popupX/shared/Card';
 import Page from '@popup/popupX/shared/Page';
 import { useSelectedAccountInfo } from '@popup/shared/AccountInfoListenerContext/AccountInfoListenerContext';
+import { useBlockChainParametersAboveV0 } from '@popup/shared/BlockChainParametersProvider';
 
-import AccountCooldowns from '../AccountCooldowns';
-import { showValidatorRestake, showValidatorAmount, showValidatorOpenStatus } from './util';
-import { ValidationResultLocationState } from './Result';
+import AccountCooldowns from '../../AccountCooldowns';
+import { showValidatorRestake, showValidatorAmount, showValidatorOpenStatus, isRange } from '../util';
+import { ValidationResultLocationState } from '../Result';
 
 const REMOVE_STATE: ValidationResultLocationState = {
     type: 'remove',
@@ -22,6 +23,7 @@ export default function ValidatorStatus() {
     const { t } = useTranslation('x', { keyPrefix: 'earn.validator' });
     const accountInfo = useSelectedAccountInfo();
     const nav = useNavigate();
+    const chainParams = useBlockChainParametersAboveV0();
 
     if (accountInfo?.type !== AccountInfoType.Baker) {
         return <Navigate to=".." />;
@@ -29,13 +31,13 @@ export default function ValidatorStatus() {
 
     const { accountBaker, accountCooldowns } = accountInfo;
 
-    if (accountBaker.version === 0) {
+    if (accountBaker.version === 0 || chainParams === undefined) {
         // assume protocol version >= 4
         return null;
     }
 
     return (
-        <Page>
+        <Page className="validator-status">
             <Page.Top heading={t('status.title')} />
             <Card>
                 <Card.Row>
@@ -45,13 +47,15 @@ export default function ValidatorStatus() {
                     />
                 </Card.Row>
                 <Card.Row>
-                    <Card.RowDetails title={t('values.id.label')} value={accountBaker.bakerId.toString()} />
-                </Card.Row>
-                <Card.Row>
                     <Card.RowDetails
                         title={t('values.restake.label')}
                         value={showValidatorRestake(accountBaker.restakeEarnings)}
                     />
+                </Card.Row>
+            </Card>
+            <Card className="validator-status__info">
+                <Card.Row>
+                    <Card.RowDetails title={t('values.id.label')} value={accountBaker.bakerId.toString()} />
                 </Card.Row>
                 <Card.Row>
                     <Card.RowDetails
@@ -65,6 +69,30 @@ export default function ValidatorStatus() {
                         value={accountBaker.bakerPoolInfo.metadataUrl || ' '}
                     />
                 </Card.Row>
+                {isRange(chainParams.transactionCommissionRange) && (
+                    <Card.Row>
+                        <Card.RowDetails
+                            title={t('values.transactionFeeCommission.label')}
+                            value={`${accountBaker.bakerPoolInfo.commissionRates.transactionCommission * 100}%`}
+                        />
+                    </Card.Row>
+                )}
+                {isRange(chainParams.bakingCommissionRange) && (
+                    <Card.Row>
+                        <Card.RowDetails
+                            title={t('values.bakingRewardCommission.label')}
+                            value={`${accountBaker.bakerPoolInfo.commissionRates.bakingCommission * 100}%`}
+                        />
+                    </Card.Row>
+                )}
+                {isRange(chainParams.finalizationCommissionRange) && (
+                    <Card.Row>
+                        <Card.RowDetails
+                            title={t('values.finalizationRewardCommission.label')}
+                            value={`${accountBaker.bakerPoolInfo.commissionRates.finalizationCommission * 100}%`}
+                        />
+                    </Card.Row>
+                )}
             </Card>
             <AccountCooldowns cooldowns={accountCooldowns} />
             <Page.Footer>
