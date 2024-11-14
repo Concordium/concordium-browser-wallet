45d4d3e1ede26ad65f5e6d429a07068f18d3e711
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-11-14 10:48:16 +0100

    Add translations file

---
+++ packages/browser-wallet/src/popup/popupX/pages/IdIssuance/i18n/en.ts
@@ -0,0 +1,10 @@
+const en = {
+    idIssuer: {
+        title: 'Request an identity',
+        description:
+            'The ID Documents (e.g. Passport pictures) that are used for the ID verification, are held exclusively by our trusted, third-party identity providers in their own off-chain records.\n\nChoose one of the identity providers below to request a Concordium Identity and create an account.',
+        buttonContinue: 'Request Identity',
+    },
+};
+
+export default en;
