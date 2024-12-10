eed75c4b36711913dfec78c56656d9f83c341d2c
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-12-10 10:19:31 +0100

    Import web3id export

--- packages/browser-wallet/src/popup/popupX/pages/Web3Id/i18n/en.ts
+++ packages/browser-wallet/src/popup/popupX/pages/Web3Id/i18n/en.ts
@@ -5,8 +5,11 @@ const t = {
     },
     import: {
         importWeb3Id: 'Import Web3 ID Credentials',
-        selectFile: 'or Select file to import',
-        dragAndDropFile: 'Drag and drop here\nyour Credentials file here',
+        success: 'Import successful',
+        noCreds: 'However, no new credentials were found.',
+        buttonDone: 'Done',
+        dragAndDropFile: 'Drag and drop\nyour Credentials file here',
+        error: 'Unable to import the chosen file. The file must be a backup created with the same seed phrase.',
     },
     details: {
         title: 'Credential details',
