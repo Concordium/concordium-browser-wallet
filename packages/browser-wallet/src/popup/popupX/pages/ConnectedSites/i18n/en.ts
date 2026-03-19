5e1563174b4640170fbea7c7c4804156db2be8f0
IvanMahda <im@concordium.com>
2026-03-19 16:40:37 +0200

    [WAL-1415] New UI - Allowlist
    Added Allowlist feature that enables users to view and manage a list of pre-approved domains or dApps

--- packages/browser-wallet/src/popup/popupX/pages/ConnectedSites/i18n/en.ts
+++ packages/browser-wallet/src/popup/popupX/pages/ConnectedSites/i18n/en.ts
@@ -1,7 +1,12 @@
 const t = {
     connectedSites: 'Connected sites',
-    disconnect: 'Disconnect',
     backTitle: 'To Accounts list',
+    edit: 'Edit',
+    noConnectedSites: 'No sites connected sites',
+    allowlistEdit: 'Allowlisting a service',
+    allowlistInfo:
+        'Allowlisting a service means that it can request identity proofs and signatures from selected accounts.\n\nYou can modify which accounts are accessible to the service below. Any allowlisted service can request proofs.',
+    remove: 'Remove service from allowlist',
 };
 
 export default t;
